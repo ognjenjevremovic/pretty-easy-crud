005df64bb23a33719c917b51c93c2f2e53d32b65
ognjenjevremovic <jevremovic.ognjen@gmail.com>
2017-02-15 12:26:31 +0100

    New features and code rewrite

--- _test.js
+++ _test.js
@@ -1,11 +1,10 @@
 //  Dependancies
-var CRUD        =   require('./index'),
-    Logger      =   require('pretty-easy-logs'),
+var Mongo_CRUD  =   require('./index'),
     MongoClient =   require('mongodb').MongoClient;
 
-
-var errLog  =   new Logger(0),
-    infoLog =   new Logger({mode: 'info', includeTime : true});
+//
+// var errLog  =   new Logger(0),
+//     infoLog =   new Logger({mode: 'info', includeTime : true});
 
 //  Connect to the database
 MongoClient.connect('mongodb://localhost:27017/test', connection);
@@ -15,25 +14,25 @@ function connection(err, db) {
     'use strict';
 
 
-    //  init
-    var collection,
-        _crudSettings = {
-            info : true,
-            warn : true,
-            scs  : true
-        };
-
-
     //  Error connecting to database
     if (err) {
-        errLog(err);
+        console.log(err);
         throw new Error('\n => Error connecting to database \n'.red);
     }
 
-    //  Set collection
-    collection =    db.collection('testCol');
+
+    //  init
+    var collection, configuration;
+
+    collection    = db.collection('testCol');
+    configuration = 'all';
 
     //  Crud instance
-    var testCollection =    new CRUD(collection, {includeLogs : true});
-
+    var database =  Mongo_CRUD(configuration, collection);
+    database
+        .add('Ognjen')
+        .then(function(err, documentInserted) {
+            console.log('Document inserted successfuly!');
+            console.log(documentToInsert);
+        });
 }
